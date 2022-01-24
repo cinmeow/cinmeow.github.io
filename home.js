83615590df82d2521e3a55f9200594b7d4102151
Cindy <cindylin@Cindys-MacBook-Pro.local>
2022-01-24 16:29:22 +0800

    tab pop out

---
+++ home.js
@@ -0,0 +1,7 @@
+
+/* opening tabs on links for icons clicks */
+// This is really stupid, figure out how to call these funtions with variables based on what was clicked
+
+function newTabLinkedin() {window.open("https://www.linkedin.com/in/chih-hsien-cindy-lin-0520/", "_blank");}
+function newTabGit() {window.open("https://github.com/cinmeow", "_blank");}
+function newTabInsta() {window.open("https://www.instagram.com/_cinnomnom/?hl=en", "_blank");}
