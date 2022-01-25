48e1ef38ed222730cd58fa0a0a1204be17e2cc35
Cindy <cindylin@Cindys-MacBook-Pro.local>
2022-01-25 10:20:52 +0800

    attempt to readjust window size

--- home.js
+++ home.js
@@ -5,3 +5,15 @@
 function newTabLinkedin() {window.open("https://www.linkedin.com/in/chih-hsien-cindy-lin-0520/", "_blank");}
 function newTabGit() {window.open("https://github.com/cinmeow", "_blank");}
 function newTabInsta() {window.open("https://www.instagram.com/_cinnomnom/?hl=en", "_blank");}
+function meme() {window.open("https://www.youtube.com/watch?v=XpC04fMrhd8"), "_blank"};
+
+/* adjusting website for window size */
+/* code from: https://www.tutorialrepublic.com/faq/how-to-capture-browser-window-resize-event-in-javascript.php*/
+function adjustWindow() {
+        var w = document.documentElement.clientWidth;
+        var h = document.documentElement.clientHeight;
+        document.getElementById("result").innerHTML = "Width: " + w + ", " + "Height: " + h;
+}
+
+//window.addEventListener('resize', adjustWindow);
+//adjustWindow();
